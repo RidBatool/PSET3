c3858b7a2860ddb57dcef83d8ec9d45c7628f78a
Rida Batool <ridabatoolghori@gmail.com>
2026-01-14 22:07:43 +0500

    Task3_logic

---
+++ Task3/main.py
@@ -0,0 +1,37 @@
+def broadcast():
+    N, Q, list= map(int, input().split())
+    subscr={}
+    for u in range(N):
+        subscr[u]=set()
+
+    mgs=[]
+    msg_id=1
+    for i in range(Q):
+        act= input.split()
+        if act[0]=="S":
+            u= int(act[1])
+            v=int(act[2])
+            subscr[u].add(v)
+        elif act[0] == "U":
+            u = int(act[1])
+            v = int(act[2])
+            if v in subscr[u]:
+                subscr[u].remove(v)
+        elif act[0] == "B":
+            u = int(act[1])
+            m = int(act[2])
+            critical = m % 3 == 0
+            mgs.append((msg_id, u, critical))
+            msg_id += 1
+        elif act[0] == "F":
+            u = int(act[1])
+            feed = []
+            for mid, sender, critical in mgs:
+                if sender in subscr[u]:
+                    feed.append((mid, critical))
+            top_feed = feed[:10]
+
+            if top_feed:
+                print(" ".join(map(str, top_feed)))
+            else:
+                print("EMPTY")
